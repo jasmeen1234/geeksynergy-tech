df243fb2d4262240d12b89315d920731e057e739
jasmeen1234 <jasmeenbano12@gmail.com>
2024-04-02 13:39:46 +0530

    prefinal

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import {TextField,MenuItem,FormControl,Select,InputLabel} from '@mui/material';
+import {TextField, MenuItem, FormControl, Select, InputLabel} from '@mui/material';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
@@ -15,142 +15,128 @@ import Container from '@mui/material/Container';
 import { useNavigate } from "react-router-dom";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-
-
 const defaultTheme = createTheme();
 
 export default function Signup() {
     const [formData, setFormData] = useState({
-        name: '',
-        password: '',
+        firstName: '',
+        lastName: '',
         email: '',
+        password: '',
         phone: '',
         profession: '',
     });
-    const navigate = useNavigate();
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      let anyFieldEmpty = false;
-  
-      for (const key in formData) {
-          if (formData[key] ===" ") {
-              anyFieldEmpty = false;
-              alert('Please fill in all fields.');
-              break;
-          }
-      }
-  
-      if (!anyFieldEmpty) {
-          localStorage.setItem('userData', JSON.stringify(formData));
-          alert('Signup successful!');
-          navigate("/login");
-          
-          // Optional: Log email and password
-          const data = new FormData(event.currentTarget);
-          console.log({
-              email: data.get('email'),
-              password: data.get('password'),
-          });
-      }
-  };
-  
-  
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    
-    if (name === 'phone') {
-      // Check  value contains only digits
-      if (!(/^\d*$/).test(value)) {
-         
-          return;
-      }
-  }
-    setFormData({ ...formData, [name]: value });
-};
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  value={formData.firstName}
-                        onChange={handleChange}
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  value={formData.lastName}
-                        onChange={handleChange}
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Email"
-                  variant="outlined"
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  fullWidth
-                  margin="normal"
-                  required
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                 label="Phone number"
-                 variant="outlined"
-                 name="phone"
-                 value={formData.phone}
-                 onChange={handleChange}
-                 fullWidth
-                 margin="normal"
-                />
-              </Grid>
-              <Grid item xs={12}>
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Check if any text field is empty
+        const isAnyFieldEmpty = Object.values(formData).some(value => value === '');
+
+        if (isAnyFieldEmpty) {
+            alert('Please fill in all fields.');
+        } else {
+            localStorage.setItem('userData', JSON.stringify(formData));
+            alert('Signup successful!');
+            navigate("/login");
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === 'phone') {
+            // Check if value contains only digits
+            if (!(/^\d*$/).test(value)) {
+                return;
+            }
+        }
+        setFormData({ ...formData, [name]: value });
+    };
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    value={formData.firstName}
+                                    onChange={handleChange}
+                                    label="First Name"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label="Last Name"
+                                    name="lastName"
+                                    value={formData.lastName}
+                                    onChange={handleChange}
+                                    autoComplete="family-name"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Email"
+                                    variant="outlined"
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    fullWidth
+                                    margin="normal"
+                                    required
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Phone number"
+                                    variant="outlined"
+                                    name="phone"
+                                    value={formData.phone}
+                                    onChange={handleChange}
+                                    fullWidth
+                                    margin="normal"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
                                 <TextField
                                     select
                                     label="Profession"
@@ -168,33 +154,32 @@ export default function Signup() {
                                     <MenuItem value="teacher">Teacher</MenuItem>
                                 </TextField>
                             </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-              <Link onClick={() => navigate("/login")} variant="body2">
+                            <Grid item xs={12}>
+                                <FormControlLabel
+                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                    label="I want to receive inspiration, marketing promotions and updates via email."
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link onClick={() => navigate("/login")} variant="body2">
                                     Already have an account? Sign in
                                 </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-       
-      </Container>
-    </ThemeProvider>
-  );
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
 }
