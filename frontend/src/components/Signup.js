1bc854be9fc5f6856edf7b7392466e24059e3271
jasmeen1234 <jasmeenbano12@gmail.com>
2024-04-02 12:50:20 +0530

    nowfinal

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -178,7 +178,7 @@ export default function Signup() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-              <Link href="/login" variant="body2">
+              <Link onClick={() => navigate("/login")} variant="body2">
                                     Already have an account? Sign in
                                 </Link>
               </Grid>
